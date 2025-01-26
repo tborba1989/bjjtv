b5039526c5337839b2c60a9e14d6e49e2b2b457c
Thyago Borba <thyago.borba@vls3j.onmicrosoft.com>
2025-01-26 12:26:55 -0300

    deploy3

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      }
+    ],
+  }
 };
 
 export default nextConfig;
